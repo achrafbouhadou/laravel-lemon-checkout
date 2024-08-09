47d69725f45f34c643ac189177e754a288ed2fb9
achrafbouhadou <achraf1323@gmail.com>
2024-08-09 22:56:42 +0100

    Add image to product seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,6 +20,7 @@ class ProductSeeder extends Seeder
             [
                 'name' => 'Rich Dad Poor Dad',
                 'description' => 'Rich Dad and Poor Dad. The best book ever.',
+                'image' => 'https://m.media-amazon.com/images/I/81bsw6fnUiL._SL1500_.jpg',
                 'price' => 19.99,
             ]
         );
